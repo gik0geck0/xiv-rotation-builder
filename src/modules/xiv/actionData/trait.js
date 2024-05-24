20aae7e3778191a3badc2afb345be3b438ec22e8
Ethan Jenkins <epjenkins@mines.edu>
2024-05-24 14:36:45 -0600

    Finished the basic trait class (Not adding further functionality till reaching a stretch goal and resolving the level consideration issue)

---
+++ src/modules/xiv/actionData/trait.js
@@ -0,0 +1,9 @@
+class Trait{
+    constructor(traitImage, traitName, levelReq, effect){
+        //Varaibles from the json
+        this.traitImage = skillImage
+        this.traitName = skillName
+        this.levelReq = levelReq
+        this.effect = effect
+    }
+}
