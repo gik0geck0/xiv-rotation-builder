f74977e4cbfc97ebb266c9d90462c247c78ab821
Matt Buland <gik0geck0@gmail.com>
2024-05-28 16:05:15 -0600

    changes we worked through together

--- src/modules/xiv/iconList/iconList.js
+++ src/modules/xiv/iconList/iconList.js
@@ -9,4 +9,42 @@ export default class IconList extends LightningElement {
         // Make up a time component to use as a unique value
         return this.actionList.map((v, i) => Object.assign({t: i*2.5}, v));
     }
+
+    draggedItem;
+
+    dragStart(e) {
+        this.draggedItem = e.target;
+    }
+
+    dragEnd(e) {
+        e.preventDefault();
+        const listElements = [...this.template.querySelectorAll("li:not(.dragging)"),];
+        const currentIndex = listElements.findIndex((e) => e === this.draggedItem);
+        const destinationIndex = this.getClosestItemIndex(listElements, e.clientX);
+
+        if (destinationIndex < 0) {
+            if (e.clientX < this.draggedItem.getBoundingClientRect().left) {
+                destinationIndex = 0;
+            } else {
+                destinationIndex = listElements.length;
+            }
+        } 
+
+        const movedItem = this.actionList.splice(currentIndex, 1)[0];
+        // TODO: the size of the list has changed, check a<b or a>b to adjust destinationIndex accordingly
+        this.actionList.splice(destinationIndex, 0, movedItem);
+        this.draggedItem = null;
+    }
+
+    dragOver(e) {
+        // show cursor for drop location?
+    }
+
+    getClosestItemIndex(haystack, needle) {
+        return haystack.findIndex((item) => {
+            const box = item.getBoundingClientRect();
+            return needle > box.left && needle < box.right;
+        });
+    }
+
 }
