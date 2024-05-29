13798b62a457ebe6a85caa530c0008a3d0e99aad
Matt Buland <gik0geck0@gmail.com>
2024-05-29 10:15:03 -0600

    changes we worked through together (#12)
    Looks Good!

--- src/modules/xiv/jobIcon/jobIcon.js
+++ src/modules/xiv/jobIcon/jobIcon.js
@@ -6,14 +6,6 @@ export default class JobIcon extends LightningElement {
     @api actionId;
     @api hoverText;
 
-    constructor(name, url, cd, rc){
-        // SHOULD Look up from a skillslist or from the JSON file eventually
-        this.name = name;
-        this.url = url;
-        this.cooldown = cd;
-        this.recast = rc;
-    }
-
     // onclick
     // ondelete
     // ondrag?
@@ -24,16 +16,5 @@ export default class JobIcon extends LightningElement {
 
     get computedSize() {
         return "height:3em;margin:3em;"
-    }
-
-    generateIcon(){
-        const newImg = document.createElement('img');
-        newImg.setAttribute('draggable', 'false');
-        newImg.setAttribute('src', this.url);
-        const newListItem = document.createElement('li');
-        newListItem.setAttribute('draggable', 'true');
-        newListItem.setAttribute('class', 'JobIcon');
-        newListItem.appendChild(newImg);
-        return newListItem;
     }
 }
--- src/modules/xiv/rotationBuilder/rotationBuilder.js
+++ src/modules/xiv/rotationBuilder/rotationBuilder.js
@@ -1,75 +1,11 @@
 import { LightningElement } from 'lwc';
 import { getActionInfo } from 'xiv/actionRepository';
-import JobIcon from '../jobIcon/jobIcon';
-
-const sortableList = document.getElementById("sortable");
-const buttonGCD = document.getElementById('addGCDButton');
-const buttonWeave = document.getElementById('addWeaveButton');
-let draggedItem = null;
 
 export default class HelloWorldApp extends LightningElement {
     mockActionList = ["Holy Spirit", "Holy Spirit", "Holy Spirit"].map(getActionInfo.bind(undefined, "pld"));
     job = "pld";
-}
 
-buttonGCD.addEventListener('click', () => {
-    const newGCD = new JobIcon("Holy Spirit", 'https://lds-img.finalfantasyxiv.com/d/b8ad098c5afaad79a847f3fb79298456c1e9d689.png', 2.5, 0);
-    IconList = [...document.querySelectorAll('JobIcon')];
-    sortableList.appendChild(newGCD.generateIcon());
-  }
-);
-
-sortableList.addEventListener(
-	"dragstart",
-	(e) => {
-		draggedItem = e.target;
-		setTimeout(() => {
-			e.target.style.display = "none";
-		}, 0);
-    }
-);
-
-sortableList.addEventListener(
-	"dragend",
-	(e) => {
-		setTimeout(() => {
-			e.target.style.display = "";
-			draggedItem = null;
-		}, 0);
-    }
-);
-
-sortableList.addEventListener(
-	"dragover",
-	(e) => {
-		e.preventDefault();
-		const afterElement = getDragAfterElement(sortableList , e.clientX);
-		const currentElement = document.querySelector(".dragging");
-		if (afterElement == null) {
-			sortableList.appendChild(draggedItem);
-        } 
-		else {
-			sortableList.insertBefore(draggedItem , afterElement);
-        }
+	addHolySpirit() {
+		this.mockActionList.push(getActionInfo(this.job, "Holy Spirit"));
 	}
-);
-
-const getDragAfterElement = (container, x) => {
-	const draggableElements = [...container.querySelectorAll("li:not(.dragging)"),];
-
-	return draggableElements.reduce(
-		(closest, child) => {
-			const box = child.getBoundingClientRect();
-			const offset = x - box.left - box.width / 2;
-			if (offset < 0 && offset > closest.offset) {
-				return { offset: offset, element: child,};
-            } 
-			else {
-				return closest;
-			}
-        },
-		{
-			offset: Number.NEGATIVE_INFINITY,
-		}
-	).element;
-};
+}
