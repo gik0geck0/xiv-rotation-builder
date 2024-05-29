40eeb08846d8114af43099a8973f803ee8b3e9d3
Aarnav Singh <aarnavsingh@mines.edu>
2024-05-29 18:54:20 -0500

    Fixed Timeline logic

--- src/modules/xiv/rotationBuilder/rotationBuilder.js
+++ src/modules/xiv/rotationBuilder/rotationBuilder.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 import { getActionInfo } from 'xiv/actionRepository';
 
 export default class HelloWorldApp extends LightningElement {
-    mockActionList = ["Holy Spirit", "Holy Spirit", "Holy Spirit"].map(getActionInfo.bind(undefined, "pld"));
+    mockActionList = ["Holy Spirit", "Holy Spirit", "Holy Spirit", ""].map(getActionInfo.bind(undefined, "pld"));
     job = "pld";
 
 	addHolySpirit() {
