edbcfff090131c64e6efaf4af4d29760913f2636
Aarnav Singh <118383692+ThinkingTurtle@users.noreply.github.com>
2024-05-30 12:39:26 -0500

    Timeline integration (#16)
    * Insert the bare minimum for the timeline needs re-factoring
    
    * Added more timeline code that's poorly integrated. I'm struggling to integrate the code, will ask questions next Tuesday
    
    * Formatting changes and small corrections
    
    * Added horizontal scrolling to the timeline
    
    * changes we worked through together (#12)
    
    Looks Good!
    
    * Fixed Timeline logic
    
    * deleted null icon
    
    ---------
    
    Co-authored-by: Matt Buland <gik0geck0@gmail.com>

--- src/modules/xiv/iconList/iconList.js
+++ src/modules/xiv/iconList/iconList.js
@@ -9,4 +9,44 @@ export default class IconList extends LightningElement {
         // Make up a time component to use as a unique value
         return this.actionList.map((v, i) => Object.assign({t: i*2.5}, v));
     }
+
+    draggedItem;
+    tempActionList;
+
+    dragStart(e) {
+        this.draggedItem = e.target;
+    }
+
+    dragEnd(e) {
+        e.preventDefault();
+        let tempActionList = [...this.actionList];
+        const listElements = [...this.template.querySelectorAll("xiv-job-icon"),];
+        const currentIndex = listElements.findIndex((e) => e === this.draggedItem);
+        let destinationIndex = this.getClosestItemIndex(listElements, e.clientX);
+
+        if (destinationIndex < 0) {
+            if (e.clientX < this.template.querySelector(".sortable-list").getBoundingClientRect().left) {
+                destinationIndex = 0;
+            } else {
+                destinationIndex = listElements.length;
+            }
+        } 
+        const movedItem = tempActionList.splice(currentIndex, 1)[0];
+        //@api can't be modified by the component through modifiying functions like splice
+        tempActionList.splice(destinationIndex, 0, movedItem);
+        this.actionList = tempActionList;
+        this.draggedItem = null;
+    }
+
+    dragOver(e) {
+        // show cursor for drop location?
+    }
+
+    getClosestItemIndex(haystack, needle) {
+        return haystack.findIndex((item) => {
+            const box = item.getBoundingClientRect();
+            return needle > box.left && needle < box.right;
+        });
+    }
+
 }
