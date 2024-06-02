689a513e79b5f290c8950f86ea48ed3a8c991661
Aarnav Singh <aarnavsingh@mines.edu>
2024-06-02 16:32:49 -0500

    I got all the Icon in the tray to work as buttons and append to the timeline

--- src/modules/xiv/iconTray/iconTray.js
+++ src/modules/xiv/iconTray/iconTray.js
@@ -4,17 +4,12 @@ import { getJobActions } from 'xiv/actionRepository';
 
 export default class IconTray extends LightningElement {
     @api job;
-    @api actionList;
 
     jobActions = getJobActions("paladin");
 
     addAction(e){
-        let tempActionList = [...this.actionList];
         const listElements = [...this.template.querySelectorAll("xiv-job-icon")];
         const currentIndex = listElements.findIndex((e) => e == e.target);
-        console.log(getActionInfo("paladin", e.target.name));
-        tempActionList.push(getActionInfo("paladin", e.target.name));
-        console.log(tempActionList);
-        this.actionList = tempActionList;
+        this.dispatchEvent(new CustomEvent('addaction', {detail: {actionName: e.target.name}}));
     }
 }
