a6951906f6c35e3530b80c3601c3af87dfd281a8
Justin Colman <jcolman@mines.edu>
2024-06-03 15:04:24 -0600

    Added comments and placeholder time

--- src/modules/xiv/rotationBuilder/rotationBuilder.js
+++ src/modules/xiv/rotationBuilder/rotationBuilder.js
@@ -15,8 +15,7 @@ export default class HelloWorldApp extends LightningElement {
     
 
     calculatePotency(actionList, job){
-        console.log(actionList);
-        let currTime = 1
+        let currTime = 0;
         //Calculation
         let totalPotency = 0;
         let currBuffs = [];
@@ -24,23 +23,29 @@ export default class HelloWorldApp extends LightningElement {
         let lastAction = {};
         let extraPotency = null;
         let stacksUsed = 0;
+        //Go through every item in the list
         for (let i = 0; i < actionList.length; i++ ){
             buffAmt = 1;
             let currAction = actionList[i]
+            //check if it starts a % based buff
             if(currAction.hasOwnProperty("buff")){
                 currBuffs.push([currAction,currTime,currTime+parseInt(currAction.duration)])
             }
+            //check if it grants anything
             if(currAction.hasOwnProperty("grants")){
                 for(let k = 0; k <Object.keys(currAction.grants).length; k++){
                     currBuffs.push([Object.keys(currAction.grants)[k], parseInt((currAction.grants[Object.keys(currAction.grants)[k]]))])
                 }
             }
+            //goes through the list of buffs to see if any are active
             for(let j = 0; j<currBuffs.length; j++ ){
+                //3 length means a start and end time
                 if(currBuffs[j].length == 3){
                     if(currTime <= currBuffs[j][2]){
                         buffAmt = parseFloat(currBuffs[j][0].buff);
                     }
                 }
+                //2 length means a stack based buff
                 else{
                     currBuffs[j][1] += stacksUsed;
                     if(currBuffs[j][1] >= 1){
@@ -49,26 +54,32 @@ export default class HelloWorldApp extends LightningElement {
                     stacksUsed = 0;
                 }
             }
-
+            //loop for GCD Actions
             if(currAction.type == "Spell" || currAction.type == "Weaponskill"){
+                //if there is a special potency
                 if(extraPotency != null && currAction.hasOwnProperty([extraPotency])){
                     totalPotency += (parseInt(currAction[extraPotency])) * buffAmt;
                     stacksUsed = -1;
                 }
+                //if there is a combo potency
                 else if(currAction.comboAction == lastAction.name && currAction.hasOwnProperty("comboAction")){
                     totalPotency += (parseInt(currAction.comboPotency)) * buffAmt;
                 }
+                //normal potency
                 else{
                     totalPotency += (parseInt(currAction.potency)) * buffAmt;
                 }
+                //save the last action for combo checking
                 lastAction = currAction;
+                currTime += 2.5;
             }
+            //if non-GCD and it has a potency, add that potency
             if (currAction.type == "Ability"){
                 if(currAction.hasOwnProperty("potency")){
                     totalPotency += (parseInt(currAction.potency)) * buffAmt;
                 }
             }
-
+            //reset extra potency
             extraPotency = null;
         }
 
