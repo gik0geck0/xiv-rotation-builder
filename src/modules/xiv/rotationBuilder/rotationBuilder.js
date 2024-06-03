3fb8a83f84c19f8cb9c74e529caf2cae120253ef
Aarnav Singh <118383692+ThinkingTurtle@users.noreply.github.com>
2024-06-03 11:00:27 -0500

    U/thinkingturtle/icon tray (#17)
    * The icon tray generates and I can click on the icons. I just can't figure out the logic to update our mockActionList. I think I used the same logic ans the Icon List so it should work but its not...
    
    * The icon tray generates and I can click on the icons. I just can't figure out the logic to update our mockActionList. I think I used the same logic ans the Icon List so it should work but its not...
    
    * I got all the Icon in the tray to work as buttons and append to the timeline

--- src/modules/xiv/rotationBuilder/rotationBuilder.js
+++ src/modules/xiv/rotationBuilder/rotationBuilder.js
@@ -2,11 +2,17 @@ import { LightningElement } from 'lwc';
 import { getActionInfo } from 'xiv/actionRepository';
 
 export default class HelloWorldApp extends LightningElement {
-    mockActionList = ["Holy Spirit", "Holy Spirit", "Holy Spirit"].map(getActionInfo.bind(undefined, "pld"));
-    job = "pld";
+	mockActionList = ["Holy Spirit", "Holy Spirit", "Holy Spirit", "Fast Blade"].map(getActionInfo.bind(undefined, "paladin"));
+    job = "paladin";
 
 	addHolySpirit() {
 		this.mockActionList.push(getActionInfo(this.job, "Holy Spirit"));
+		this.mockActionList = [...this.mockActionList];
+	}
+
+	addTimelineAction(e) {
+		this.mockActionList.push(getActionInfo(this.job, e.detail.actionName));
+		this.mockActionList = [...this.mockActionList];
 	}
 }
 
