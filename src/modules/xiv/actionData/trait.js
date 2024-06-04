b5ecf7a90c44b43dcc6b79ca61d47a343465a7cd
Justincolman <118383638+Justincolman@users.noreply.github.com>
2024-06-04 12:19:48 -0600

    Delete src/modules/xiv/actionData/trait.js
    Removed unused file

--- src/modules/xiv/actionData/trait.js
+++
@@ -1,9 +0,0 @@
-class Trait{
-    constructor(traitImage, traitName, levelReq, effect){
-        //Varaibles from the json
-        this.traitImage = skillImage
-        this.traitName = skillName
-        this.levelReq = levelReq
-        this.effect = effect
-    }
-}
