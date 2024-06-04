11a8ecd2dc25c3c21e90bd1b75fdd965231f9323
Aarnav Singh <aarnavsingh@mines.edu>
2024-06-04 11:49:12 -0600

    Formatting

--- src/modules/xiv/rotationBuilder/rotationBuilder.js
+++ src/modules/xiv/rotationBuilder/rotationBuilder.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 import { getActionInfo } from 'xiv/actionRepository';
 
 export default class HelloWorldApp extends LightningElement {
-	mockActionList = ["Holy Spirit", "Holy Spirit", "Holy Spirit", "Fast Blade"].map(getActionInfo.bind(undefined, "paladin"));
+	mockActionList = [].map(getActionInfo.bind(undefined, "paladin"));
     job = "paladin";
 
 	addHolySpirit() {
