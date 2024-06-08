0cc7addb86b40e9cc5aebedbf169c575299b1cb7
Ethan Jenkins <epjenkins@mines.edu>
2024-06-08 16:20:30 -0600

    Readded invalid location now including the different highlighting for abilities.

--- src/modules/xiv/jobIcon/jobIcon.js
+++ src/modules/xiv/jobIcon/jobIcon.js
@@ -39,18 +39,26 @@ export default class JobIcon extends LightningElement {
         else if (this.location == "selected"){
             if(this.type == "Ability"){
                 return "cursor: grab; display: inline-flex; height:50px; width:50px; margin:0px; padding:0px; background: skyblue; vertical-align: top; margin-top: 10px;";
-        }
+            }
             else{
                 return "cursor: grab; display: inline-flex; height:70px; width:70px; margin:0px; padding:0px; background: skyblue; margin-top: 30px;";
-         }
+            }
+        }
+        else if (this.location == "invalid"){
+            if(this.type == "Ability"){
+                return "cursor: grab; display: inline-flex; height:50px; width:50px; margin:0px; padding:0px; background: red; vertical-align: top; margin-top: 10px;";
+            }
+            else{
+                return "cursor: grab; display: inline-flex; height:70px; width:70px; margin:0px; padding:0px; background: red; margin-top: 30px;";
+            }
         }
         else{
             if(this.type == "Ability"){
                 return "cursor: grab; display: inline-flex; height:50px; width:50px; margin:0px; padding:0px; vertical-align: top;  margin-top: 10px;";
-        }
+            }
             else{
                 return "cursor: grab; display: inline-flex; height:70px; width:70px; margin:0px; padding:0px; margin-top: 30px;";
-         }
+            }
         }
     }
 }
