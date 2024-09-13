6956ef2aa925971bc7d29082a84bae3d143c431c
Bryson Smith <ixbryson@gmail.com>
2024-09-13 10:26:38 -0600

    Updated XIVFinal.json. It appears to be broken Initial ChatGPT typescript commit. I spent quite a while polishing the top 5 directories, the remaining 3 (especially rotationBuilder) need more attention Fixed all remaining red highlighting except for those related to LWC components Fixed remaining errors, the project builds now. LWR still has issues with module resolution

---
+++ src/modules/xiv/defaultSnippets/defaultSnippets.ts
@@ -0,0 +1,9 @@
+// defaultSnippets.ts
+
+import pldData from './jobSnippets/pld/pld.json';  // Import the JSON
+import { JobResources } from 'xiv/pldTypes';  // Import the interfaces
+
+// Define DefaultJobResources object with type
+export const DefaultJobResources: { [job: string]: JobResources } = {
+    PLD: pldData as JobResources  // Cast the JSON data to the JobResources interface
+};
---
+++ src/modules/xiv/iconTray/iconTray.ts
@@ -0,0 +1,30 @@
+// iconTray.ts
+import { LightningElement, api } from 'lwc';
+import { getJobActions } from 'xiv/actionRepository';
+import { Action } from 'xiv/actionDataTypes';
+
+export default class IconTray extends LightningElement {
+    @api job: string = 'paladin';
+    @api jobActions: Action[] = getJobActions('paladin');
+
+    displayDetails(e: Event) {
+        const target = e.target as HTMLElement;
+        this.dispatchEvent(
+            new CustomEvent('displaydetail', {
+                detail: {
+                    actionName: target.getAttribute('name'),
+                    actionDescription: target.getAttribute('hovertext')
+                }
+            })
+        );
+    }
+
+    addAction(e: Event) {
+        const target = e.target as HTMLElement;
+        this.dispatchEvent(
+            new CustomEvent('addaction', {
+                detail: { actionName: target.getAttribute('name') }
+            })
+        );
+    }
+}
