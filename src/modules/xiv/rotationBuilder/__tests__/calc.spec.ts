ef46c38db721367f605ef6d138d6ddab9002536f
tbray0 <tbray@mines.edu>
2024-09-22 11:29:19 -0600

    Added sanity check

---
+++ src/modules/xiv/rotationBuilder/__tests__/calc.spec.ts
@@ -0,0 +1,26 @@
+// import { LightningElement } from 'lwc';
+// import { getActionInfo } from 'xiv/actionRepository';
+// import { getJobNames } from 'xiv/actionRepository';
+// import { getJobActions } from 'xiv/actionRepository';
+// import { JobGuide } from 'xiv/actionData';
+// import HelloWorldApp  from '../rotationBuilder';
+// import { parseEffect } from '../../actionData/parseEffect';
+
+describe('xiv/rotationBuilder', () => {
+
+    beforeEach(() => {
+        
+    });
+
+    afterEach(() => {
+
+        jest.clearAllMocks();
+    });
+
+
+    describe('sanity check', () => {
+        it('should work', () => {
+            expect(true).toEqual(true);
+        });
+    });
+});
