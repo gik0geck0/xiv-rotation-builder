39fe7ccbeb1613dd26cdcdbc82d5af584178e2aa
Seth Motta <112897748+AvanShade@users.noreply.github.com>
2024-10-18 07:42:00 -0600

    Parse floats for cast, recast, duration, potency, comboPotency, comboBonus, and grants and assign numeric attribues

--- src/modules/xiv/actionData/actionData.ts
+++ src/modules/xiv/actionData/actionData.ts
@@ -26,6 +26,66 @@ for (const jobName in jobGuideJson) {
         Effect: ${action.effect}`;
         parseEffect(action);
 
+        // parseFloat of cast, recast, duration, potency, comboPotency, comboBonus, and grants if action has them
+        if(action.cast) {
+            const parsedCast = parseFloat(action.cast);
+            action.castNumeric = !isNaN(parsedCast) ? parsedCast : 0;
+        }
+
+        if(action.recast) {
+            const parsedRecast = parseFloat(action.recast);
+            action.recastNumeric = !isNaN(parsedRecast) ? parsedRecast : 0;
+        }
+
+        if(action.duration) {
+            const parsedDuration = parseFloat(action.duration);
+            action.durationNumeric = !isNaN(parsedDuration) ? parsedDuration : 0;
+        }
+
+        if(action.potency) {
+            const parsedPotency = parseFloat(action.potency);
+            action.potencyNumeric = !isNaN(parsedPotency) ? parsedPotency : 0;
+        }
+
+        if(action.comboPotency) {
+            const parsedComboPotency = parseFloat(action.comboPotency);
+            action.comboPotencyNumeric = !isNaN(parsedComboPotency) ? parsedComboPotency : 0;
+        }
+
+        if(action.comboBonus) {
+            const comboBonusNumeric = {};
+
+            for(const key in action.comboBonus) {
+                const value = action.comboBonus[key];
+                const parsedValue = parseFloat(value);
+
+                if(!isNaN(parsedValue)) {
+                    comboBonusNumeric[key] = parsedValue;
+                } else {
+                    comboBonusNumeric[key] = 0;
+                }
+            }
+
+            action.comboBonusNumeric = comboBonusNumeric;
+        }
+
+        if(action.grants) {
+            const grantsNumeric = {};
+
+            for(const key in action.grants) {
+                const value = action.grants[key];
+                const parsedValue = parseFloat(value);
+
+                if(!isNaN(parsedValue)) {
+                    grantsNumeric[key] = parsedValue;
+                } else {
+                    grantsNumeric[key] = 0;
+                }
+            }
+
+            action.grantsNumeric = grantsNumeric;
+        }
+
         // Set boolean flags for Action type and cast time
         action.isInstant = action.cast === 'Instant';
         action.isAbility = action.type === 'Ability';
